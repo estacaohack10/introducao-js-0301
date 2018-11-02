c0cb8fd5479a17df39d5020b2078c07dce4bd4aa
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-02 13:18:16 -0300

    Adicionar resolução de exercício

---
+++ soma.js
@@ -0,0 +1,15 @@
+/* 
+   RECEBA 2 VALORES NUMÉRICOS DO USUÁRIO E 
+   RETORNE UM ALERT COM O RESULTADO DA SOMA
+
+   Entrada:
+   prompt('SUA MENSAGEM')
+
+   Saída:
+   alert(RESULTADO DA SOMA);
+*/
+
+let valor1 = Number(prompt('Diga um valor:'));
+let valor2 = Number(prompt('Diga outro valor:'));
+
+alert(`Sua soma equivale a: ${valor1 + valor2}`);
